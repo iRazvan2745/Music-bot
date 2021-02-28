dba1873e6fc545c330189e627a7c676930e52634
iRazvan2745 <76774976+iRazvan2745@users.noreply.github.com>
2021-02-28 21:31:04 +0200

    Add files via upload

---
+++ config/bot.js
@@ -0,0 +1,17 @@
+module.exports = {
+    emojis: {
+        off: ':x:',
+        error: ':warning:',
+        queue: ':bar_chart:',
+        music: ':musical_note:',
+        success: ':white_check_mark:',
+    },
+
+    discord: {
+        token: 'ODExMjQ4NDgyNzIyNTEyOTI3.YCvcBg.J7Oy4NIYP0pOecjwx5bKgtvdX3U',
+        prefix: '&',
+        activity: 'Prefix & use &help for infoY',
+    },
+
+    filters: ['8D', 'gate', 'haas', 'phaser', 'treble', 'tremolo', 'vibrato', 'reverse', 'karaoke', 'flanger', 'mcompand', 'pulsator', 'subboost', 'bassboost', 'vaporwave', 'nightcore', 'normalizer', 'surrounding'],
+};
